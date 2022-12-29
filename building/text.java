d6574538bc001d03d83ca0c6a40e306f2c48796d
kode <kodelakshminarendar@gmail.com>
2022-12-28 19:31:40 -0500

    new code

---
+++ building/text.java
@@ -0,0 +1,10 @@
+public Stactic void main(){
+    for (int i=2;i<=n;i++)
+        {
+            if (i%n==0)
+                printf("Hello World");
+        }
+    else{
+        printf("good bye");
+        }
+        }
